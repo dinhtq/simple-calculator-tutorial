5983c248ea4c338fc3b954d30b72db57c8515198
Jason Dinh <jason.dinh@crowdstrike.com>
2022-01-20 16:37:01 -0800

    add click event listener and log

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log('Hello World')
+var calcContainerEle = document.querySelector('#calculator-container')
+calcContainerEle.addEventListener('click', e => onButtonClick(e))
+
+function onButtonClick(e) {
+  console.log('btnClickedHtml', e.target.innerHTML)
+}
