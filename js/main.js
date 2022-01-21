3cfad66d851893844c6315ced54f6bb5968975ca
Jason Dinh <jason.dinh@crowdstrike.com>
2022-01-20 21:32:40 -0800

    call doNumber,reset(), switch statement, etc…

--- js/main.js
+++ js/main.js
@@ -1,6 +1,64 @@
 var calcContainerEle = document.querySelector('#calculator-container')
 calcContainerEle.addEventListener('click', e => onButtonClick(e))
 
+function doNumber(num) {
+  console.log('num', num)
+}
+
+function doOperation(operation) {
+  console.log('operation', operation)
+}
+
+function reset() {
+  console.log('reset')
+}
+
+function makePosNeg() {
+  console.log('makePosNeg')
+}
+
+function makeDecimal() {
+  console.log('makeDecimal')
+}
+
+function doEqual() {
+  console.log('doEqual')
+}
+
 function onButtonClick(e) {
-  console.log('btnClickedHtml', e.target.innerHTML)
+  var btnClickedValue = e.target.innerHTML
+
+  if (Number.isInteger(parseInt(btnClickedValue))) {
+    doNumber(btnClickedValue)
+    return
+  }
+  
+  switch (btnClickedValue) {
+    case 'AC':
+      reset()
+      break
+    case '+/-':
+      makePosNeg()
+      break
+    case '/':
+      doOperation('/')
+      break
+    case 'x':
+      doOperation('x')
+      break
+    case '-':
+      doOperation('-')
+      break
+    case '+':
+      doOperation('+')
+      break
+    case '.':
+      makeDecimal()
+      break
+    case '=':
+      doEqual()
+      break
+    default:
+      break
+  }
 }
