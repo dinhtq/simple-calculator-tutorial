2f6cc758f7c0e6dfdde92169e69f6bdf7cde0369
Jason Dinh <jason.dinh@crowdstrike.com>
2022-01-21 15:09:17 -0800

    adding two numbers

--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
 var calcContainerEle = document.querySelector('#calculator-container')
 calcContainerEle.addEventListener('click', e => onButtonClick(e))
 
+var resultEle = document.querySelector('#result')
+
+var curOperation, curNumber1, curNumber2
+
+function compute(num1, num2, op) {
+  var n1 = Number(num1)
+  var n2 = Number(num2)
+  var result
+  if (op === '+') {
+    result = n1 + n2
+  } else if (op === '-') {
+    result = n1 - n2
+  } else if (op === '/') {
+    result = n1 / n2
+  } else {
+    // must be multiply
+    result = n1 * n2
+  }
+  return result
+}
+
 function doNumber(num) {
-  console.log('num', num)
+  var newNum
+  if (curOperation) {
+    curNumber2 = curNumber2 ? `${curNumber2}${num}` : num
+    newNum = curNumber2
+  } else {
+    curNumber1 = curNumber1 ? `${curNumber1}${num}` : num
+    newNum = curNumber1
+  }
+  resultEle.innerHTML = newNum
 }
 
 function doOperation(operation) {
-  console.log('operation', operation)
+  // if curNumber2 is active, then must be a continous operation (1 + 4 - 3),
+  // so perform curOperation on num1 and num2 and assign result to num1
+  if (curNumber2) {
+    var result = compute(curNumber1, curNumber2, curOperation)
+    curNumber1 = result
+    curNumber2 = 0
+    resultEle.innerHTML = result
+  }
+
+  curOperation = operation
 }
 
 function reset() {
@@ -22,7 +60,10 @@ function makeDecimal() {
 }
 
 function doEqual() {
-  console.log('doEqual')
+  if (curNumber1 && curNumber2 && curOperation) {
+    var result = compute(curNumber1, curNumber2, curOperation)
+    resultEle.innerHTML = result
+  }
 }
 
 function onButtonClick(e) {
